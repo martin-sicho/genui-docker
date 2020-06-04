71938b8b5059c9566e4715f8238c8f2653bc25d0
martin-sicho <sicho.martin@gmail.com>
2020-06-04 09:36:06 +0200

    handle some security issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,8 +16,14 @@ python manage.py collectstatic --no-input
 # ensure that only the genui user group has access to media files
 mkdir -p ${GENUI_MEDIA_ROOT}
 chgrp -R ${GENUI_USER_GROUP} ${GENUI_MEDIA_ROOT}
-chmod g+rwxs,a+xs ${GENUI_MEDIA_ROOT} # FIXME: here we allow all users to list directories and read the media files -> less permission headaches setting up the web server, but it is a security concern in some deployment settings
+chmod g+rwxs,a+xs ${GENUI_MEDIA_ROOT} # FIXME: here we allow all users to list directories and read the media files by default -> this could be a possible security concern for some sensitive files
 umask 002
+
+# disable listing of models and compounds media files
+mkdir -p "${GENUI_MEDIA_ROOT}models"
+mkdir -p "${GENUI_MEDIA_ROOT}compounds"
+chmod o-r "${GENUI_MEDIA_ROOT}models"
+chmod o-r "${GENUI_MEDIA_ROOT}compounds"
 
 # use authbind to allow the genui user group to bind the 443 port
 touch /etc/authbind/byport/443
