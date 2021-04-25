78c7bb167532403202c081551d1c545acd385665
martin-sicho <sicho.martin@gmail.com>
2021-04-25 18:38:48 +0200

    pass build arguments correctly

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ TAG=${GENUI_DOCKER_IMAGE_TAG:-"latest"}
 NVIDIA_CUDA_RUNFILE=${NVIDIA_CUDA_RUNFILE:-"./config/nvidia/cuda.run"}
 
 docker build -t ${REPO_PREFIX}/genui-base:${TAG} -f Dockerfile-base .
-docker build --build-arg NVIDIA_CUDA_RUNFILE=${NVIDIA_CUDA_RUNFILE} -t ${REPO_PREFIX}/genui-base-cuda:${TAG} -f Dockerfile-base-cuda .
-docker build -t ${REPO_PREFIX}/genui-main:${TAG} -f Dockerfile-main .
-docker build -t ${REPO_PREFIX}/genui-worker:${TAG} -f Dockerfile-worker .
-docker build -t ${REPO_PREFIX}/genui-gpuworker:${TAG} -f Dockerfile-gpuworker .
+docker build --build-arg GENUI_DOCKER_IMAGE_PREFIX=${REPO_PREFIX} --build-arg GENUI_DOCKER_IMAGE_TAG=${TAG} --build-arg NVIDIA_CUDA_RUNFILE=${NVIDIA_CUDA_RUNFILE} -t ${REPO_PREFIX}/genui-base-cuda:${TAG} -f Dockerfile-base-cuda .
+docker build --build-arg GENUI_DOCKER_IMAGE_PREFIX=${REPO_PREFIX} --build-arg GENUI_DOCKER_IMAGE_TAG=${TAG} -t ${REPO_PREFIX}/genui-main:${TAG} -f Dockerfile-main .
+docker build --build-arg GENUI_DOCKER_IMAGE_PREFIX=${REPO_PREFIX} --build-arg GENUI_DOCKER_IMAGE_TAG=${TAG} -t ${REPO_PREFIX}/genui-worker:${TAG} -f Dockerfile-worker .
+docker build --build-arg GENUI_DOCKER_IMAGE_PREFIX=${REPO_PREFIX} --build-arg GENUI_DOCKER_IMAGE_TAG=${TAG} -t ${REPO_PREFIX}/genui-gpuworker:${TAG} -f Dockerfile-gpuworker .
