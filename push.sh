2b5a51d2b53a189cc5c111db2765f3dfe068482f
martin-sicho <sicho.martin@gmail.com>
2021-06-09 15:42:46 +0200

    print the commands that will be run before running them

--- push.sh
+++ push.sh
@@ -6,4 +6,6 @@ GENUI_DOCKER_IMAGE_PREFIX=${GENUI_DOCKER_IMAGE_PREFIX:-"sichom"}
 TAG=${GENUI_DOCKER_IMAGE_TAG:-"latest"}
 NVIDIA_CUDA_VERSION=${NVIDIA_CUDA_VERSION:-""}
 
+echo "The following commands will be run:"
+echo "$(NVIDIA_CUDA_VERSION=${NVIDIA_CUDA_VERSION} python3 get_tags.py $TAG $TAG)"
 eval "$(NVIDIA_CUDA_VERSION=${NVIDIA_CUDA_VERSION} python3 get_tags.py $TAG $TAG)"
