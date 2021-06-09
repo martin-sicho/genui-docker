5fa74c05bcb9a029afab9f6ba9b50bb93b2f62be
martin-sicho <sicho.martin@gmail.com>
2021-06-09 16:26:53 +0200

    make the build script more user-friendly

--- build.sh
+++ build.sh
@@ -2,12 +2,50 @@
 
 set -e
 
-REPO_PREFIX=${GENUI_DOCKER_IMAGE_PREFIX:-"sichom"}
-TAG=${GENUI_DOCKER_IMAGE_TAG:-"latest"}
-NVIDIA_CUDA_RUNFILE=${NVIDIA_CUDA_RUNFILE:-"./config/nvidia/cuda.run"}
+while getopts ":r:t:c:" opt; do
+  case $opt in
+    r) REPO="$OPTARG"
+    ;;
+    t) TAG="$OPTARG"
+    ;;
+    c) CUDA_RUNFILE="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+REPO_PREFIX=${REPO:-"sichom"}
+TAG=${TAG:-"latest"}
+CUDA_RUNFILE=${CUDA_RUNFILE:-""}
+
+printf "TAG is %s\n" "$TAG"
+printf "REPO is %s\n" "$REPO"
+if [ -z "$CUDA_RUNFILE" ]
+then
+      echo "No CUDA runfile specified. GPU images will not be built."
+else
+      printf "CUDA images will be built with the following CUDA runfile: %s\n" "$CUDA_RUNFILE"
+fi
+
+read -p "Do you want to proceed? Type YES to confirm: " userInput
+if [[ $userInput != 'YES' ]] || [[ $userInput == '' ]]; then
+   echo "Build cancelled by user."
+   exit 1
+else
+   echo "Building images..."
+fi
 
 docker build -t ${REPO_PREFIX}/genui-base:${TAG} -f Dockerfile-base .
-docker build --build-arg GENUI_DOCKER_IMAGE_PREFIX=${REPO_PREFIX} --build-arg GENUI_DOCKER_IMAGE_TAG=${TAG} --build-arg NVIDIA_CUDA_RUNFILE=${NVIDIA_CUDA_RUNFILE} -t ${REPO_PREFIX}/genui-base-cuda:${TAG} -f Dockerfile-base-cuda .
 docker build --build-arg GENUI_DOCKER_IMAGE_PREFIX=${REPO_PREFIX} --build-arg GENUI_DOCKER_IMAGE_TAG=${TAG} -t ${REPO_PREFIX}/genui-main:${TAG} -f Dockerfile-main .
 docker build --build-arg GENUI_DOCKER_IMAGE_PREFIX=${REPO_PREFIX} --build-arg GENUI_DOCKER_IMAGE_TAG=${TAG} -t ${REPO_PREFIX}/genui-worker:${TAG} -f Dockerfile-worker .
-docker build --build-arg GENUI_DOCKER_IMAGE_PREFIX=${REPO_PREFIX} --build-arg GENUI_DOCKER_IMAGE_TAG=${TAG} -t ${REPO_PREFIX}/genui-gpuworker:${TAG} -f Dockerfile-gpuworker .
+
+if [ -z "$CUDA_RUNFILE" ]
+then
+      echo "No CUDA runfile specified. GPU images were not built."
+      exit 1
+else
+      printf "Building CUDA images with the following CUDA runfile: %s\n" "$CUDA_RUNFILE"
+      docker build --build-arg GENUI_DOCKER_IMAGE_PREFIX=${REPO_PREFIX} --build-arg GENUI_DOCKER_IMAGE_TAG=${TAG} --build-arg NVIDIA_CUDA_RUNFILE=${CUDA_RUNFILE} -t ${REPO_PREFIX}/genui-base-cuda:${TAG} -f Dockerfile-base-cuda .
+      docker build --build-arg GENUI_DOCKER_IMAGE_PREFIX=${REPO_PREFIX} --build-arg GENUI_DOCKER_IMAGE_TAG=${TAG} -t ${REPO_PREFIX}/genui-gpuworker:${TAG} -f Dockerfile-gpuworker .
+fi
