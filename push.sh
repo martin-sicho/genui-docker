ae664612507763b033aa12cc26c2195b4f8bd6a0
martin-sicho <sicho.martin@gmail.com>
2021-06-09 16:06:01 +0200

    make the push script more user-friendly

--- push.sh
+++ push.sh
@@ -2,10 +2,34 @@
 
 set -e
 
-GENUI_DOCKER_IMAGE_PREFIX=${GENUI_DOCKER_IMAGE_PREFIX:-"sichom"}
-TAG=${GENUI_DOCKER_IMAGE_TAG:-"latest"}
+while getopts ":r:t:" opt; do
+  case $opt in
+    r) REPO="$OPTARG"
+    ;;
+    t) TAG="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+printf "Argument TAG is %s\n" "$TAG"
+printf "Argument REPO is %s\n" "$REPO"
 NVIDIA_CUDA_VERSION=${NVIDIA_CUDA_VERSION:-""}
+if [ -z "$NVIDIA_CUDA_VERSION" ]
+then
+      echo "No CUDA version specified."
+else
+      printf "CUDA images will be tagged with CUDA version: %s\n" "$NVIDIA_CUDA_VERSION"
+fi
 
 echo "The following commands will be run:"
-echo "$(NVIDIA_CUDA_VERSION=${NVIDIA_CUDA_VERSION} python3 get_tags.py $TAG $TAG)"
-eval "$(NVIDIA_CUDA_VERSION=${NVIDIA_CUDA_VERSION} python3 get_tags.py $TAG $TAG)"
+echo "$(NVIDIA_CUDA_VERSION=${NVIDIA_CUDA_VERSION} GENUI_DOCKER_IMAGE_PREFIX=${REPO} python3 get_tags.py $TAG $TAG)"
+
+read -p "Do you want to proceed? Type YES to confirm: " userInput
+if [[ $userInput != 'YES' ]] || [[ $userInput == '' ]]; then
+   echo "Push cancelled by user."
+else
+   echo "OK"
+   #eval "$(NVIDIA_CUDA_VERSION=${NVIDIA_CUDA_VERSION} python3 get_tags.py $TAG $TAG)"
+fi
